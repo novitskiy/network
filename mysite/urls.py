c4fa0bf6cecaa06a6217023000fe4a23035da11d
Artem Mikhalev <artem.mikhalev@phystech.edu>
2018-04-01 00:29:44 +0300

    Added simple checkout

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landing.urls')),
     path('', include('products.urls')),
-    # path('', include('orders.urls')),
+    path('', include('orders.urls')),
 ] \
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- static/static_dev/js/scripts.js
+++ static/static_dev/js/scripts.js
@@ -1,44 +1,90 @@
-$(document).ready(function(){
+$(document).ready(function() {
     var form = $('#form_buying_product');
+    //console.log(form);
 
-    form.on('submit', function(e){
+
+    function updateBasket(product_id, nmb, is_delete) {
+        var data = {};
+        data.product_id = product_id;
+        data.nmb = nmb;
+        var csrf_token = $('#form_buying_product [name="csrfmiddlewaretoken"]').val();
+        data["csrfmiddlewaretoken"] = csrf_token;
+
+        if (is_delete) {
+            data["is_delete"] = true;
+        }
+        else {
+            data["is_delete"] = false
+        }
+
+        //console.log(data)
+        var url = form.attr("action");
+
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: data,
+            cache: true,
+            success: function (data) {
+                //console.log("OK");
+                //console.log(data.products_total_nmb);
+                if (data.products_total_nmb || data.products_total_nmb == 0) {
+                    $('#basket_total_nmb').text("("+data.products_total_nmb+")");
+                    //console.log(data.products);
+                    $('.basket-items ul').html("");
+                    $.each(data.products, function(k, v) {
+                        $('.basket-items ul').append('<li>'+ v.name+', ' + v.nmb + 'шт. '
+                            + 'по ' + v.price_per_item + 'руб.  '
+                            + '<a class="delete-item" href="" data-product_id="'+v.id+'">x</a>'
+                            + '</li>'
+                        );
+                    });
+                }
+
+            },
+            error: function() {
+                console.log("error")
+            }
+        })
+    }
+
+
+    function showBasket() {
+        $('.basket-items').removeClass('hidden');
+    };
+
+
+    form.on('submit', function(e) {
         e.preventDefault();
-//        console.log('123');
         var nmb = $('#number').val();
-//        console.log(nmb);
         var submit_btn = $('#submit_btn');
         var product_id =  submit_btn.data("product_id");
         var name = submit_btn.data("name");
         var price = submit_btn.data("price");
-//        console.log(product_id );
-//        console.log(name);
-
-        $('.basket-items ul').append('<li>'+name+', ' + nmb + 'шт. ' + 'по ' + price + 'руб.  ' +
-            '<a class="delete-item" href="">x</a>'+
-            '</li>');
-
+        //console.log(product_id);
+        //console.log(name);
+        updateBasket(product_id, nmb, is_delete=false)
     });
 
-    function showingBasket(){
-        $('.basket-items').removeClass('hidden');
-    };
-
-    //$('.basket-container').on('click', function(e){
+    //$('.basket-container').on('click', function(e) {
     //    e.preventDefault();
     //    showingBasket();
     //});
 
-     $('.basket-container').mouseover(function(){
-         showingBasket();
+     $('.basket-container').mouseover(function() {
+         showBasket();
      });
 
-     //$('.basket-container').mouseout(function(){
-     //    showingBasket();
+     //$('.basket-container').mouseout(function() {
+     //    showBasket();
      //});
 
-     $(document).on('click', '.delete-item', function(e){
+     $(document).on('click', '.delete-item', function(e) {
          e.preventDefault();
-         $(this).closest('li').remove();
+         product_id = $(this).data("product_id")
+         nmb = 0;
+         updateBasket(product_id, nmb, is_delete=true)
      })
 
 });
+
